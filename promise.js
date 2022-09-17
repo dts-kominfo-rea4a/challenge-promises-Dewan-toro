63524f079169aeb1295b12cbc871e0484843c2dc
Dewan-toro <dewantoro477@gmail.com>
2022-09-17 23:48:51 +0700

    challenge submit

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    const joinTheater = theaterIXX.concat(theaterVGC);
+
+    var countEmosi = 0;
+
+    for (let i=0; i<joinTheater.length; i++){
+      if (joinTheater[i].hasil === emosi) {
+        countEmosi++;
+      }
+    }
+
+    return countEmosi;
+
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 module.exports = {
   promiseOutput,
